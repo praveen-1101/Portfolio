b74c7ec4a328abff191679e56f54a6fd88e1a51d
praveen-1101 <borapraveen1101@gmail.com>
2024-12-05 22:12:36 +0530

    initial functionality

---
+++ portfolio/script.js
@@ -0,0 +1,16 @@
+let words =document.querySelectorAll(".word");
+
+words.forEach((word)=>{
+
+let letters= word.textContent.split("");
+
+word.textContent="";
+
+letters.forEach((letter)=>{
+
+let span =document.createElement("span");
+
+span.textContent = letter;
+
+span.className = "letter";
+word.append(span);
